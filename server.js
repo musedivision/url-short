ebf00d25bb3981d11b9b110d0272a3a21cb6cc0b
musedivison <patrick.mccaffrey.19@gmail.com>
2016-11-13 10:49:57 +1300

    grabbed url param

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ var app = express()
 
 var port = process.env.PORT || 8080
 
+// instructions
 app.get('/', function(req,res){
   res.send("Katherine is better than trump")
+})
+
+// new url path
+app.get('/new/*', function (req,res) {
+  console.log(req.params[0])
+  res.send("URL shortener: " + req.params[0])
 })
 
 app.listen(port, function(){
